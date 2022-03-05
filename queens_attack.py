afba158f150ce74d64cc9f7e50ea8208949e6103
Luke Bakken <luke@bakken.io>
2022-03-05 06:24:43 -0800

    version 1

--- queens_attack.py
+++ queens_attack.py
@@ -1,9 +1,7 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
+import pprint
 import sys
 
 #
@@ -17,32 +15,64 @@ import sys
 #  4. INTEGER c_q
 #  5. 2D_INTEGER_ARRAY obstacles
 #
+# int n: the number of rows and columns in the board
+# nt k: the number of obstacles on the board
+# int r_q: the row number of the queen's position - 1 based
+# int c_q: the column number of the queen's position - 1 based
+# int obstacles[k][2]: each element is a list of integers, the row and column of an obstacle
+
+def is_valid_pos(n, pos):
+    r = pos[0]
+    c = pos[1]
+    return r >= 1 and c >= 1 and r <= n and c <= n
+
+def is_not_blocked(obst, pos):
+    return pos not in obst
+
+def calc_pos(n, obst, cur_pos, m, acc):
+    next_pos = (cur_pos[0] + m[0], cur_pos[1] + m[1])
+    if is_valid_pos(n, next_pos):
+        if is_not_blocked(obst, next_pos):
+            acc.append(next_pos)
+            return calc_pos(n, obst, next_pos, m, acc)
+        else:
+            return acc
+    else:
+        return acc
 
 def queensAttack(n, k, r_q, c_q, obstacles):
-    # Write your code here
+    obst = list(map(tuple, obstacles))
+    q_pos = (r_q, c_q)
+    n_m = (0, 1)
+    ne_m = (1, 1)
+    nw_m = (-1, 1)
+    s_m = (0, -1)
+    se_m = (1, -1)
+    sw_m = (-1, -1)
+    e_m = (1, 0)
+    w_m = (-1, 0)
+    motions = [n_m, ne_m, nw_m, s_m, se_m, sw_m, e_m, w_m]
+    total_moves = 0
+    for m in motions:
+        total_moves += len(calc_pos(n, obst, q_pos, m, []))
+    return total_moves
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    first_multiple_input = input().rstrip().split()
-
-    n = int(first_multiple_input[0])
-
-    k = int(first_multiple_input[1])
-
-    second_multiple_input = input().rstrip().split()
-
-    r_q = int(second_multiple_input[0])
-
-    c_q = int(second_multiple_input[1])
-
+    n = 0
+    k = 0
+    r_q = 0
+    c_q = 0
     obstacles = []
 
-    for _ in range(k):
-        obstacles.append(list(map(int, input().rstrip().split())))
+    with open(sys.argv[1]) as f:
+        first_multiple_input = f.readline().rstrip().split()
+        n = int(first_multiple_input[0])
+        k = int(first_multiple_input[1])
+        second_multiple_input = f.readline().rstrip().split()
+        r_q = int(second_multiple_input[0])
+        c_q = int(second_multiple_input[1])
+        for _ in range(k):
+            obstacles.append(list(map(int, f.readline().rstrip().split())))
 
     result = queensAttack(n, k, r_q, c_q, obstacles)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(result)
