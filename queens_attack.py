b006f81a098fd3bff1d695a3d96a5728d05936d3
Luke Bakken <luke@bakken.io>
2022-03-05 08:43:31 -0800

    simplify partitioning

--- queens_attack.py
+++ queens_attack.py
@@ -21,6 +21,16 @@ import sys
 # int c_q: the column number of the queen's position - 1 based
 # int obstacles[k][2]: each element is a list of integers, the row and column of an obstacle
 
+n_m = (1, 0)
+e_m = (0, 1)
+s_m = (-1, 0)
+w_m = (0, -1)
+ne_m = (1, 1)
+nw_m = (1, -1)
+se_m = (-1, 1)
+sw_m = (-1, -1)
+motions = [n_m, ne_m, nw_m, s_m, se_m, sw_m, e_m, w_m]
+
 def is_valid_pos(n, pos):
     r = pos[0]
     c = pos[1]
@@ -44,81 +54,44 @@ def calc_moves(n, obst, cur_pos, m):
             break
     return moves
 
-def partition_obst(q_pos, motions, obst):
+def partition_obst(q_pos, obst):
     q_r = q_pos[0]
     q_c = q_pos[1]
 
     obst_by_m = {}
+    for m in motions:
+        obst_by_m[m] = []
+
     for o in obst:
         o_r = o[0]
         o_c = o[1]
-        for m in motions:
-            if m not in obst_by_m:
-                obst_by_m[m] = []
-            m_r = m[0]
-            m_c = m[1]
-            if m_r == 0 and m_c == 1:
-                # this is a straight-line e motion
-                # the only obstacles that can apply have the same
-                # row value as the queen and greater column
-                if o_r == q_r and o_c > q_c:
-                    obst_by_m[m].append(o)
-            elif m_r == 0 and m_c == -1:
-                # this is a straight-line w motion
-                # the only obstacles that can apply have the same
-                # row value as the queen and lesser column
-                if o_r == q_r and o_c < q_c:
-                    obst_by_m[m].append(o)
-            elif m_r == 1 and m_c == 0:
-                # this is a straight-line n motion
-                # the only obstacles that can apply have the same
-                # column value as the queen and greater row
-                if o_c == q_c and o_r > q_r:
-                    obst_by_m[m].append(o)
-            elif m_r == -1 and m_c == 0:
-                # this is a straight-line s motion
-                # the only obstacles that can apply have the same
-                # column value as the queen and less row
-                if o_c == q_c and o_r < q_r:
-                    obst_by_m[m].append(o)
-            elif m_r == 1 and m_c == 1:
-                # ne obstacles
-                if o_c > q_c and o_r > q_r:
-                    obst_by_m[m].append(o)
-            elif m_r == 1 and m_c == -1:
-                # nw obstacles
-                if o_c < q_c and o_r > q_r:
-                    obst_by_m[m].append(o)
-            elif m_r == -1 and m_c == 1:
-                # se obstacles
-                if o_c > q_c and o_r < q_r:
-                    obst_by_m[m].append(o)
-            elif m_r == -1 and m_c == -1:
-                # sw obstacles
-                if o_c < q_c and o_r < q_r:
-                    obst_by_m[m].append(o)
-            else:
-                raise RuntimeError('partition_obst unreachable')
+        if o_r == q_r and o_c > q_c:
+            obst_by_m[e_m].append(o)
+        elif o_r == q_r and o_c < q_c:
+            obst_by_m[w_m].append(o)
+        elif o_c == q_c and o_r > q_r:
+            obst_by_m[n_m].append(o)
+        elif o_c == q_c and o_r < q_r:
+            obst_by_m[s_m].append(o)
+        elif o_c > q_c and o_r > q_r:
+            obst_by_m[ne_m].append(o)
+        elif o_c < q_c and o_r > q_r:
+            obst_by_m[nw_m].append(o)
+        elif o_c > q_c and o_r < q_r:
+            obst_by_m[se_m].append(o)
+        elif o_c < q_c and o_r < q_r:
+            obst_by_m[sw_m].append(o)
 
     return obst_by_m
 
 def queensAttack(n, k, r_q, c_q, obstacles):
     obst = list(map(tuple, obstacles))
     q_pos = (r_q, c_q)
-    n_m = (1, 0)
-    e_m = (0, 1)
-    s_m = (-1, 0)
-    w_m = (0, -1)
-    ne_m = (1, 1)
-    nw_m = (1, -1)
-    se_m = (-1, 1)
-    sw_m = (-1, -1)
-    motions = [n_m, ne_m, nw_m, s_m, se_m, sw_m, e_m, w_m]
-    obst_by_m = partition_obst(q_pos, motions, obst)
+    obst_by_m = partition_obst(q_pos, obst)
     total_moves = 0
     # print('q_pos: {}'.format(pprint.pformat(q_pos)))
     for m in motions:
-        m_obst = obst_by_m[m]
+        m_obst = obst_by_m.get(m, [])
         # print('m: {}'.format(pprint.pformat(m)))
         # print('m_obst: {}'.format(pprint.pformat(m_obst)))
         total_moves += calc_moves(n, m_obst, q_pos, m)
