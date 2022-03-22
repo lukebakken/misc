2b07932dfc88ea1910c53c4a0f4dd1cf82d74462
Luke Bakken <luke@bakken.io>
2022-03-22 09:24:43 -0700

    pwsh

--- erl/pwsh.erl
+++ erl/pwsh.erl
@@ -3,36 +3,43 @@
 -export([run/0]).
 
 run() ->
-    Pwsh = os:find_executable("pwsh.exe"), 
+    SystemRootDir = os:getenv("SystemRoot", "/"), 
+    Pwsh = os:find_executable("powershell.exe"), 
     
-    A1 = ["-NonInteractive","-NoProfile","-NoLogo","-InputFormat","Text","-WindowStyle","Hidden"],
+    A1 = ["-NonInteractive", "-NoProfile", "-NoLogo", "-InputFormat", "Text", "-WindowStyle", "Hidden"],
     
-    A0 = [use_stdio,stderr_to_stdout,overlapped_io,{line,512},hide,{args, A1}],
+    % Note: 'hide' must be used or this will not work!
+    A0 = [use_stdio, stderr_to_stdout, {cd, SystemRootDir}, {line, 512}, hide, {args, A1}],
 
     P = erlang:open_port({spawn_executable, Pwsh}, A0),
+    MonRef = erlang:monitor(port, P),
 
     Cmd = "(Get-PSDrive -Name C).Free",
     true = erlang:port_command(P, Cmd ++ "\n"),
-    sh_receive(P, Cmd),
+    sh_receive(MonRef, P, Cmd),
     true = erlang:port_command(P, Cmd ++ "\n"),
-    sh_receive(P, Cmd),
+    sh_receive(MonRef, P, Cmd),
 
     timer:sleep(2500), 
 
     true = erlang:port_command(P, Cmd ++ "\n"),
-    sh_receive(P, Cmd),
+    sh_receive(MonRef, P, Cmd),
     true = erlang:port_command(P, Cmd ++ "\n"),
-    sh_receive(P, Cmd),
+    sh_receive(MonRef, P, Cmd),
 
-    ExitCmd = "exit 0",
-    true = erlang:port_command(P, ExitCmd ++ "\n"),
+    % ExitCmd = "exit 0",
+    % true = erlang:port_command(P, ExitCmd ++ "\n"),
 
+    true = erlang:demonitor(MonRef, [flush]),
     RV = erlang:port_close(P),
     io:format("port_close RV: ~p~n", [RV]).
 
 
-sh_receive(Port, Cmd) ->
+sh_receive(MonRef, Port, Cmd) ->
     FoundCmd =  receive
+                    {'DOWN', MonRef, port, Port, Info0} ->
+                        io:format("Saw unexpected DOWN message! ~p~n", [Info0]);
+                        % TODO: restart, re-try
                     {Port, {data, {eol, Data0}}} ->
                         io:format("Data0: ~p~n", [Data0]),
                         case string:find(Data0, Cmd, trailing) of
@@ -48,6 +55,9 @@ sh_receive(Port, Cmd) ->
     case FoundCmd of
         ok ->
             receive
+                {'DOWN', MonRef, port, Port, Info1} ->
+                    io:format("Saw unexpected DOWN message! ~p~n", [Info1]);
+                    % TODO: restart, re-try
                 {Port, {data, {eol, Data1}}} ->
                     io:format("Data: ~p~n", [Data1])
             after 5000 ->
