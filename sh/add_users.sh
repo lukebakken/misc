4bff9e95342f4cb349184855d1060392fb160991
Luke Bakken <luke@bakken.io>
2023-12-30 16:25:40 -0800

    tweaks

--- sh/add_users.sh
+++ sh/add_users.sh
@@ -7,9 +7,13 @@ set -o pipefail
 
 while IFS=$'\t' read -r name password
 do
+    echo "USER: $name"
     set +o errexit
     /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl add_user "$name" "$password"
     set -o errexit
+    /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl set_permissions "$name" '.*' '.*' '.*'
+    /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl list_user_permissions "$name"
+    echo
 done < 'users.txt'
 
 /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl list_users
