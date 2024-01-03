c57364bea7cc7f15e4acbe1eec3be665db9d2a1b
Luke Bakken <luke@bakken.io>
2024-01-02 16:31:05 -0800

    more add_users stuff

--- sh/add_users.sh
+++ sh/add_users.sh
@@ -10,9 +10,11 @@ do
     echo "USER: $name"
     set +o errexit
     /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl add_user "$name" "$password"
+    /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl authenticate_user "$name" "$password"
     set -o errexit
     /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl set_permissions "$name" '.*' '.*' '.*'
     /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl list_user_permissions "$name"
+    /home/lbakken/development/rabbitmq/rabbitmq-server/sbin/rabbitmqctl change_password "$name" "$password"
     echo
 done < 'users.txt'
 
