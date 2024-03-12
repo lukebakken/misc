9456f35525b240b38e5533c48404072393271e3c
Luke Bakken <luke@bakken.io>
2024-03-12 07:24:02 -0700

    Logging

--- java/basic_publish/src/main/java/io/bakken/basic_publish/App.java
+++ java/basic_publish/src/main/java/io/bakken/basic_publish/App.java
@@ -4,26 +4,44 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.AMQP.Queue.DeclareOk;
+import java.time.ZonedDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class App 
 {
+    public static String now()
+    {
+        return ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
+    }
+
     public static void main(String[] argv) throws Exception {
+        final byte[] body = "HELLO WORLD".getBytes();
         final ConnectionFactory factory = new ConnectionFactory();
         factory.setRequestedHeartbeat(5);
         factory.setHost(System.getenv("RMQ_HOST"));
+        factory.setPort(55672);
 		factory.setUsername("guest");
         factory.setPassword("guest");
         factory.setVirtualHost("/");
         final Connection connection = factory.newConnection();
         final Channel channel = connection.createChannel();
+        channel.confirmSelect();
+
+        channel.addConfirmListener(
+            (deliveryTag, multiple) ->
+                System.out.printf("[INFO] %s confirmed: %d%n", now(), deliveryTag),
+            (deliveryTag, multiple) ->
+                System.out.printf("[INFO] %s nacked: %d%n", now(), deliveryTag)
+        );
 
         DeclareOk q = channel.queueDeclare();
-        System.out.printf("[INFO] publishing messages to %s, start toxic at any point", q.getQueue());
+        System.out.printf("[INFO] %s publishing messages to %s, start toxic at any point",
+                now(), q.getQueue());
 
         int i = 0;
-        byte[] body = "HELLO WORLD".getBytes();
         while (true) {
-            System.out.printf("[INFO] publishing message %d%n", i);
+            System.out.printf("[INFO] %s publishing message %d%n", now(), i);
             channel.basicPublish("", q.getQueue(), null, body);
             i++;
             Thread.sleep(1000);
