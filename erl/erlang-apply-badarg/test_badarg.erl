3e0ab6faf9df1c0f83cb3a23b1e012a9f77a9b66
Luke Bakken <luke@bakken.io>
2024-09-20 16:06:21 -0700

    Getting closer

--- erl/erlang-apply-badarg/test_badarg.erl
+++ erl/erlang-apply-badarg/test_badarg.erl
@@ -3,7 +3,18 @@
 -export([run/0]).
 
 run() ->
-    State = [],
-    Mod = <<"mod_one">>,
-    Mod:is_empty(State),
-    init:stop().
+    Mod = 1234,
+    Mod:is_empty(),
+    halt().
+
+%% reason: {badarg,
+%%             [{erlang,apply,
+%%                  [is_empty,[]],
+%%                  [{error_info,#{module => erl_erts_errors}}]},
+%%              {rabbit_mirror_queue_slave,process_instruction,2,
+%%                  [{file,"rabbit_mirror_queue_slave.erl"},{line,992}]},
+%%              {rabbit_mirror_queue_slave,handle_cast,2,
+%%                  [{file,"rabbit_mirror_queue_slave.erl"},{line,359}]},
+%%              {gen_server2,handle_msg,2,
+%%                  [{file,"gen_server2.erl"},{line,1056}]},
+%%              {proc_lib,wake_up,3,[{file,"proc_lib.erl"},{line,251}]}]}
